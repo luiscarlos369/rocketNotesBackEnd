3c705296f380e84901b80a6ea20d3d81f76356a8
18lc18 <solis18.serv@icloud.com>
2023-06-02 20:22:43 -0300

    second commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv/config");
 require("express-async-errors");
 const cors = require("cors");
 
@@ -42,5 +43,5 @@ message: "Internal server error"
 
 
 
-const PORT = 3693;
+const PORT = process.env.PORT || 3693;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
